3d53fc07d1b7e79e6e35a1561009f0d3803fe454
T. Lanette <TLPollard4516@gmail.com>
2023-10-08 21:38:40 -0400

    Update: created Navbar
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -1,19 +1,87 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
-
+import { useEffect, useState } from 'react';
 
 
 const Navbar = () => {
-    return (
-        <nav className='navigation'>
-            <Link to='/'>
-                <img className='company-logo' src='/public/assets/shared/desktop/logo.svg' alt='coffee bean icon' />
-            </Link>
-            <ul>
+    const [isNotMobile, setIsNotMobile] = useState(window.innerWidth > 767);
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
 
-            </ul>
-        </nav>
-    )
+    const updateMobile = () => {
+        setIsNotMobile(window.innerWidth > 767);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', updateMobile);
+        return () => window.removeEventListener('resize', updateMobile);
+    });
+
+    const isNavOpen = () => {
+        setIsNavExpanded(true);
+    };
+
+    const isNavClosed = () => {
+        setIsNavExpanded(false);
+    }
+
+    return (
+        <>
+            {isNotMobile ? (
+                <nav className='navbar'>
+                    <Link to='/'>
+                        <img className='company-logo' src='/public/assets/shared/desktop/logo.svg' alt='coffee roasters logo' />
+                    </Link>
+                    <div className='navbar-menu'>
+                        <ul>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About Us</Link>
+                            </li>
+                            <li>
+                                <Link to='/subscribe'>Create Your Plan</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            ) : (
+                <>
+                    <nav className='mobile-nav'>
+                        <Link to='/'>
+                            <img className='company-logo' src='/public/assets/shared/desktop/logo.svg' alt='coffee roasters logo' />
+                        </Link>
+                        {isNavExpanded ? (
+                            <svg
+                                onClick={isNavClosed}
+                                width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M12.657.843a1.5 1.5 0 010 2.121L9.12 6.5l3.536 3.536a1.5 1.5 0 11-2.121 2.12L7 8.622l-3.536 3.536a1.5 1.5 0 11-2.12-2.121L4.877 6.5 1.343 2.964A1.5 1.5 0 113.464.844L7 4.377 10.536.843a1.5 1.5 0 012.12 0z" fill="#333D4B" fill-rule="evenodd" /></svg>
+                        ) : (
+                            <svg
+                                onClick={isNavOpen}
+                                className='hamburger-icon'
+                                width="16" height="15" xmlns="http://www.w3.org/2000/svg"><path d="M14.5 12a1.5 1.5 0 010 3h-13a1.5 1.5 0 010-3h13zm0-6a1.5 1.5 0 010 3h-13a1.5 1.5 0 010-3h13zm0-6a1.5 1.5 0 010 3h-13a1.5 1.5 0 010-3h13z" fill="#333D4B" fill-rule="evenodd" /></svg>
+                        )}
+                    </nav>
+                    {isNavExpanded ? (
+                        <nav className='mobile-nav-menu'>
+                            <ul>
+                                <li>
+                                    <Link to='/'>Home</Link>
+                                </li>
+                                <li>
+                                    <Link to='/about'>About</Link>
+                                </li>
+                                <li>
+                                    <Link to='/subscribe'>Create Your Plan</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    ) : (
+                        ""
+                    )}
+                </>
+            )}
+        </>
+    );
 }
 
 export default Navbar;
