09209f5f5ab86194e501830cd056e4ed71313ccd
T. Lanette <TLPollard4516@gmail.com>
2023-10-16 16:18:28 -0400

    Updates: added third section
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HomeBanner } from './HomeBanner';
 import { OurCollection } from './OurCollection';
 import { WhyChoose } from './WhyChoose';
+import { HowItWorks } from './HowItWorks';
 
 export function Home() {
     return (
@@ -9,6 +10,7 @@ export function Home() {
             <HomeBanner />
             <OurCollection />
             <WhyChoose />
+            <HowItWorks />
         </>
     );
 }
