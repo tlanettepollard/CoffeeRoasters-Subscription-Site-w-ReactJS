cfb5624efd1aa1095e9993f0d104c33db8efaf52
T. Lanette <TLPollard4516@gmail.com>
2023-10-16 14:13:17 -0400

    Added section 3 of home page
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home/WhyChoose.jsx
+++ src/pages/Home/WhyChoose.jsx
@@ -6,11 +6,27 @@ export function WhyChoose() {
                 <p className="choose-body">A large part of our role is choosing which particular coffees will be featured in our range. This means working closely with the best coffee growers to give you a more impactful experience on every level.</p>
             </div>
             <div className="choose-container">
-                <div className="choose-card">
-                    <img src="/public/assets/home/desktop/icon-coffee-bean.svg" alt="coffee bean icon" className="choose-image" />
-                    <div className="choose-card-copy">
+                <div className="card">
+                    <img src="/public/assets/home/desktop/icon-coffee-bean.svg" alt="coffee bean icon" className="card-image" />
+                    <div className="card-copy">
                         <p className="card-title">Best quality</p>
                         <p className="card-body">Discover an endless variety of the world’s best artisan coffee from each of our roasters.</p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src="/public/assets/home/desktop/icon-gift.svg" alt="coffee bean icon" className="card-image" />
+                    <div className="card-copy">
+                        <p className="card-title">Exclusive benefits</p>
+                        <p className="card-body">Special offers and swag when you subscribe, including 30% off your first shipment. </p>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <img src="/public/assets/home/desktop/icon-truck.svg" alt="coffee bean icon" className="card-image" />
+                    <div className="card-copy">
+                        <p className="card-title">Free shipping</p>
+                        <p className="card-body">We cover the cost and coffee is delivered fast. </p>
                     </div>
                 </div>
             </div>
