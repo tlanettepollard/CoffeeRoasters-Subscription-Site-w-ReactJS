b3dadcb7d1b83289e675e0bd32a3e5f58a50b111
T. Lanette <TLPollard4516@gmail.com>
2023-10-16 13:07:35 -0400

    Update: added banner section for home;
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home/HomeBanner.jsx
+++ src/pages/Home/HomeBanner.jsx
@@ -1,1 +1,28 @@
+import { useNavigate } from "react-router-dom";
 
+export function HomeBanner() {
+    let navigate = useNavigate();
+    const changeRoute = () => {
+        let path = '/subscribe';
+        navigate(path);
+    }
+
+
+    return (
+        <div className="home-banner">
+            <div className="home-banner-image" style={{ backgroundImage: "url('/public/assets/home/mobile/image-hero-coffeepress.jpg')" }}>
+                <div className="home-banner-container">
+                    <p className="home-banner-title">
+                        Great coffee <br /> made simple.
+                    </p>
+                    <p className="home-banner-body">
+                        Start your mornings with the world’s best coffees. Try our expertly curated artisan coffees from our best roasters delivered directly to your door, at your schedule.
+                    </p>
+                    <button onClick={changeRoute} className="primary-button">
+                        Create your plan
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+};
