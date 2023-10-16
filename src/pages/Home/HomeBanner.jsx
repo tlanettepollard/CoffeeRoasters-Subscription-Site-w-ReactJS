f228f42c11da8fd325cee462cef10577df89ff97
T. Lanette <TLPollard4516@gmail.com>
2023-10-16 13:25:39 -0400

    Added background image based on media queries
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home/HomeBanner.jsx
+++ src/pages/Home/HomeBanner.jsx
@@ -7,10 +7,27 @@ export function HomeBanner() {
         navigate(path);
     }
 
+    const css = `@media (max-width: 767px) {
+        .home-banner-image {
+            background-image: url('/public/assets/home/mobile/image-hero-coffeepress.jpg')
+        }
+    }
+    @media (min-width: 768px) {
+        .home-banner-image {
+            background-image: url('/public/assets/home/tablet/image-hero-coffeepress.jpg')
+        }
+    }
+    @media (min-width: 1200px) {
+        .home-banner-image {
+            background-image: url('/public/assets/home/desktop/image-hero-coffeepress.jpg');
+        }
+    }`;
+
 
     return (
         <div className="home-banner">
-            <div className="home-banner-image" style={{ backgroundImage: "url('/public/assets/home/mobile/image-hero-coffeepress.jpg')" }}>
+            <style scoped>{css}</style>
+            <div className="home-banner-image">
                 <div className="home-banner-container">
                     <p className="home-banner-title">
                         Great coffee <br /> made simple.
