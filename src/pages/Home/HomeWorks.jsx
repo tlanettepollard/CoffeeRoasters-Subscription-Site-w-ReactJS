e36bff44f43e26caaa61fca38461619a041c3370
T. Lanette <TLPollard4516@gmail.com>
2023-10-21 21:50:00 -0400

    Updates: styled home works section
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home/HomeWorks.jsx
+++ src/pages/Home/HomeWorks.jsx
@@ -13,21 +13,21 @@ export function HomeWorks() {
                 <p className="works-title">How it works</p>
                 <div className="works-container">
                     <div className="works-card">
-                        <div className="works-number">01</div>
-                        <div className="works-step">Pick your coffee</div>
-                        <div className="works-detail">Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.</div>
+                        <div className="works-card-number">01</div>
+                        <div className="works-card-step">Pick your coffee</div>
+                        <div className="works-card-detail">Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.</div>
                     </div>
 
                     <div className="works-card">
-                        <div className="works-number">02</div>
-                        <div className="works-step">Choose the frequency</div>
-                        <div className="works-detail">Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.</div>
+                        <div className="works-card-number">02</div>
+                        <div className="works-card-step">Choose the frequency</div>
+                        <div className="works-card-detail">Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.</div>
                     </div>
 
                     <div className="works-card">
-                        <div className="works-number">03</div>
-                        <div className="works-step">Receive and enjoy!</div>
-                        <div className="works-detail">We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.</div>
+                        <div className="works-card-number">03</div>
+                        <div className="works-card-step">Receive and enjoy!</div>
+                        <div className="works-card-detail">We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.</div>
                     </div>
                 </div>
                 <button onClick={changeRoute} className="button-home-works">
