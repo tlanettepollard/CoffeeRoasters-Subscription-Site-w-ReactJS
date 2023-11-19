dcd3e18b7275545d00c2f25c47dd458ffca0706e
T. Lanette <TLPollard4516@gmail.com>
2023-11-19 14:38:10 -0500

    Updates: About Section 3
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/About/AboutHeadQtrs.jsx
+++ src/pages/About/AboutHeadQtrs.jsx
@@ -2,10 +2,51 @@ export function AboutHeadQtrs() {
     return (
         <>
             <div className="about-section3">
-                <div className="about-headquarters-title">
+                <div className="about-headqtrs-title">
                     Our headquarters
                 </div>
+                <div className="about-headqtrs-container">
+                    <div className="about-hq-card">
+                        <img src="/public/assets/about/desktop/illustration-uk.svg" alt="map of United Kingdom Illustration" className="about-hq-card-image" />
+                        <div className="about-hq-card-copy">
+                            <p className="about-hq-card-title">United Kingdom</p>
+                            <div className="about-hq-card-body">
+                                <span>68 Asfordby Rd</span>
+                                <span>Alcaston</span>
+                                <span>SY6 1YA</span>
+                                <span>+44 1241 918425</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="about-hq-card">
+                        <img src="" alt="" className="about-hq-card-image" />
+                        <div className="about-hq-card-copy">
+                            <p className="about-hq-card-title"></p>
+                            <div className="about-hq-card-body">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="about-hq-card">
+                        <img src="" alt="" className="about-hq-card-image" />
+                        <div className="about-hq-card-copy">
+                            <p className="about-hq-card-title"></p>
+                            <div className="about-hq-card-body">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </>
     );
 }
