ca4809324c0292c6ba8b8c069e2b3d315cc0861d
T. Lanette <TLPollard4516@gmail.com>
2023-11-19 19:41:07 -0500

    Added ordersummary section
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ src/pages/Subscribe/SubscribeSecTwo/SubOrderSummary.jsx
@@ -0,0 +1,31 @@
+export function SubOrderSummary({
+    drink, type, quantity, grind, frequency, disabled,
+}) {
+    return (
+        <>
+            <div className="order-summary-container">
+                <div className="order-summary">
+                    <p className="order-summary-title">Order Summary</p>
+                </div>
+                {disabled ? (
+                    <p className='order-summary-text'>
+                        “I drink coffee as{" "}
+                        <span className='order-selection'>{drink}</span> , with a{" "}
+                        <span className='order-selection'>{type}</span> type of bean.{" "}
+                        <span className='order-selection'>{quantity}</span>, sent to me
+                        <span className='order-selection'>{frequency}</span>."
+                    </p>
+                ) : (
+                    <p className='order-summary-text'>
+                        “I drink my coffee as{" "}
+                        <span className='order-selection'>{drink}</span> , with a{" "}
+                        <span className='order-selection'>{type}</span> type of bean.{" "}
+                        <span className='order-selection'>{quantity}</span> ground ala{" "}
+                        <span className='order-selection'>{grind}</span>, sent to me
+                        <span className='order-selection'>{frequency}</span>."
+                    </p>
+                )}
+            </div>
+        </>
+    );
+}
