a10aefe9a17f8ce6c2699b96c81bd9d120d2668a
T. Lanette <TLPollard4516@gmail.com>
2023-11-22 22:30:24 -0500

    Added Accordion
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ src/pages/Subscribe/SubscribeSecTwo/SubAccordion.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+
+export function SubAccordion({
+    option,
+    changeWord,
+    setMenuActive,
+    defaultChecked,
+}) {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleActiveBtn = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <div className={defaultChecked === "" ? 'accordion-item' : active ? 'accordion-item' : hidden}>
+            <div className="accordion-header" onClick={toggleActiveBtn}>
+                <h1>{option.title}</h1>
+                <img src="/public/assets/plan/desktop/icon-arrow.svg" alt="" />
+            </div>
+            <div className="accordion-body">
+                <label className='label' htmlFor={option.optionOne.title}>
+                    <input
+                        className='radio'
+                        type='radio'
+                        name={option.id}
+                        id={option.optionOne.title}
+                        value={option.optionOne.title}
+                        onChange={(e) => changeWord(e.target.value)}
+                        checked={defaultChecked === 'Capsule' ? 'defaultChecked' : null}
+                        onClick={setMenuActive}
+                    />
+                    <div className="label-container label-container-one">
+                        <p className="label-title">{option.optionOne.title}</p>
+                        <p className='label-description'>{option.optionOne.description}</p>
+                    </div>
+                </label>
+
+                <label className='label' htmlFor={option.optionTwo.title}>
+                    <input
+                        className='radio'
+                        type='radio'
+                        name={option.id}
+                        id={option.optionTwo.title}
+                        value={option.optionTwo.title}
+                        onChange={(e) => changeWord(e.target.value)}
+                        checked={defaultChecked === 'Capsule' ? 'defaultChecked' : null}
+                        onClick={setMenuActive}
+                    />
+                    <div className="label-container">
+                        <p className="label-title">{option.optionTwo.title}</p>
+                        <p className='label-description'>{option.optionTwo.description}</p>
+                    </div>
+                </label>
+
+                <label className='label' htmlFor={option.optionThree.title}>
+                    <input
+                        className='radio'
+                        type='radio'
+                        name={option.id}
+                        id={option.optionThree.title}
+                        value={option.optionThree.title}
+                        onChange={(e) => changeWord(e.target.value)}
+                        checked={defaultChecked === 'Capsule' ? 'defaultChecked' : null}
+                        onClick={setMenuActive}
+                    />
+                    <div className="label-container">
+                        <p className="label-title">{option.optionThree.title}</p>
+                        <p className='label-description'>{option.optionThree.description}</p>
+                    </div>
+                </label>
+            </div>
+        </div>
+    );
+}
