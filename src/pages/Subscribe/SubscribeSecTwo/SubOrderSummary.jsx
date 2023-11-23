517f2aa812b759f124561be9ec832535bb665745
T. Lanette <TLPollard4516@gmail.com>
2023-11-22 19:35:37 -0500

    Updates: order summary
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Subscribe/SubscribeSecTwo/SubOrderSummary.jsx
+++ src/pages/Subscribe/SubscribeSecTwo/SubOrderSummary.jsx
@@ -6,25 +6,26 @@ export function SubOrderSummary({
             <div className="order-summary-container">
                 <div className="order-summary">
                     <p className="order-summary-title">Order Summary</p>
+
+                    {disabled ? (
+                        <p className='order-summary-text'>
+                            “I drink coffee as{" "}
+                            <span className='order-selection'>{drink}</span> , with a{" "}
+                            <span className='order-selection'>{type}</span> type of bean.{" "}
+                            <span className='order-selection'>{quantity}</span>, sent to me
+                            <span className='order-selection'>{frequency}</span>."
+                        </p>
+                    ) : (
+                        <p className='order-summary-text'>
+                            “I drink my coffee as{" "}
+                            <span className='order-selection'>{drink}</span> , with a{" "}
+                            <span className='order-selection'>{type}</span> type of bean.{" "}
+                            <span className='order-selection'>{quantity}</span> ground ala{" "}
+                            <span className='order-selection'>{grind}</span>, sent to me
+                            <span className='order-selection'>{frequency}</span>."
+                        </p>
+                    )}
                 </div>
-                {disabled ? (
-                    <p className='order-summary-text'>
-                        “I drink coffee as{" "}
-                        <span className='order-selection'>{drink}</span> , with a{" "}
-                        <span className='order-selection'>{type}</span> type of bean.{" "}
-                        <span className='order-selection'>{quantity}</span>, sent to me
-                        <span className='order-selection'>{frequency}</span>."
-                    </p>
-                ) : (
-                    <p className='order-summary-text'>
-                        “I drink my coffee as{" "}
-                        <span className='order-selection'>{drink}</span> , with a{" "}
-                        <span className='order-selection'>{type}</span> type of bean.{" "}
-                        <span className='order-selection'>{quantity}</span> ground ala{" "}
-                        <span className='order-selection'>{grind}</span>, sent to me
-                        <span className='order-selection'>{frequency}</span>."
-                    </p>
-                )}
             </div>
         </>
     );
--- src/router.jsx
+++ src/router.jsx
@@ -4,7 +4,7 @@ import { Navbar } from "./layout/Navbar/Navbar";
 import { Footer } from "./layout/Footer/Footer";
 import { Home } from "./pages/Home/Home";
 import { About } from "./pages/About/About";
-import { Subscribe } from "./pages/Subscribe/Subscribe";
+import { SubscribePlan } from "./pages/Subscribe/SubscribePlan";
 
 
 export const router = createBrowserRouter([
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
         children: [
             { path: '/', element: <Home /> },
             { path: '/about', element: <About /> },
-            { path: '/subscribe', element: <Subscribe /> },
+            { path: '/subscribe', element: <SubscribePlan /> },
         ],
     },
     {
