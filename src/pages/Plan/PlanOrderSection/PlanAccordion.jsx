2c15a4515bb93a8741146daaec7b48ec498f40d0
T. Lanette <TLPollard4516@gmail.com>
2023-12-03 20:12:32 -0500

    Updates: made modal
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ src/pages/Plan/PlanOrderSection/PlanAccordion.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from 'react';
+import { PlanChoices } from './planData';
+
+
+export function PlanAccordion() {
+    return (
+        <div className="accordion-container">
+
+        </div>
+    );
+}
