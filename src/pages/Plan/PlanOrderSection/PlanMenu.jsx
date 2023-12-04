e94b6e265077929291bb2efdf9bf71d76e6384fe
T. Lanette <TLPollard4516@gmail.com>
2023-12-03 19:25:41 -0500

    Updates: recoding accordion section
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Plan/PlanOrderSection/PlanMenu.jsx
+++ src/pages/Plan/PlanOrderSection/PlanMenu.jsx
@@ -1,64 +1,38 @@
-import React from 'react';
-import { MenuChoices } from './planData';
-
-{/* New Menu 
-
 export function PlanMenu() {
     return (
-        <div className="plan-menu-left">
-            <ul className='plan-menu'>
-                {MenuChoices.map((choice) => {
-                    return (
-                        <div key={choice.id}>
-                            <li className='li-selected'>
-                                <h4 className="li-selected-title">
-                                    <span className="li-selected-number">{choice.number}</span>{choice.title}
-                                </h4>
-                            </li>
-                        </div>
-                    );
-                })}
+        <aside aria-label='aside navigation' className='plan-nav'>
+            <ul className="plan-nav-list">
+                <li className="plan-nav-item">
+                    <a href="#preferences">
+                        <span className="nav-item-num">01</span>
+                        <span className="nav-item-text">Preferences</span>
+                    </a>
+                </li>
+                <li className="plan-nav-item">
+                    <a href="#bean">
+                        <span className="nav-item-num">02</span>
+                        <span className="nav-item-text">Bean Type</span>
+                    </a>
+                </li>
+                <li className="plan-nav-item">
+                    <a href="#quantity">
+                        <span className="nav-item-num">03</span>
+                        <span className="nav-item-text">Quantity</span>
+                    </a>
+                </li>
+                <li className="plan-nav-item">
+                    <a href="#grind">
+                        <span className="nav-item-num">04</span>
+                        <span className="nav-item-text">Grind Option</span>
+                    </a>
+                </li>
+                <li className="plan-nav-item">
+                    <a href="#deliveries">
+                        <span className="nav-item-num">05</span>
+                        <span className="nav-item-text">Deliveries</span>
+                    </a>
+                </li>
             </ul>
-        </div>
-    );
-}
-*/ }
-
-
-{/* */ }
-export function PlanMenu({
-    menuPref,
-    menuBean,
-    menuQuantity,
-    menuGrind,
-    menuDelivery
-}) {
-    return (
-        <div className="plan-menu-left">
-            <div className="plan-menu">
-                <ul>
-                    <li id='first-menu' className={menuPref ? 'li-selected' : null}>
-                        <span className='li-number'>01</span>
-                        <span className='li-text'>Preferences</span>
-                    </li>
-                    <li className={menuBean ? 'li-selected' : null}>
-                        <span className='li-number'>02</span>
-                        <span className='li-text'>Bean type</span>
-                    </li>
-                    <li className={menuQuantity ? 'li-selected' : null}>
-                        <span className='li-number'>03</span>
-                        <span className='li-text'>Quantity</span>
-                    </li>
-                    <li className={menuGrind ? 'li-selected' : null}>
-                        <span className='li-number'>04</span>
-                        <span className='li-text'>Grind option </span>
-                    </li>
-                    <li id="fifth-menu" className={menuDelivery ? 'li-selected' : null}>
-                        <span className='li-number'>05</span>
-                        <span className='li-text'>Deliveries </span>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        </aside>
     );
 }
