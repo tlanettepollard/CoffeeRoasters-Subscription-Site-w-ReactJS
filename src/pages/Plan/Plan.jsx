112f3595ac6c2a0dca53a7e92ec6c87f942aca53
T. Lanette <TLPollard4516@gmail.com>
2023-12-30 16:03:13 -0500

    Updates: Plan page
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Plan/Plan.jsx
+++ src/pages/Plan/Plan.jsx
@@ -6,57 +6,37 @@ import { PlanMenu } from './PlanOrderSection/PlanMenu.jsx';
 import { PlanAccordion } from './PlanOrderSection/PlanAccordion.jsx';
 import { PlanOrderSummary } from './PlanOrderSection/PlanOrderSummary.jsx';
 import { PlanCheckoutModal } from './PlanOrderSection/PlanCheckoutModal.jsx';
-import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
+//import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
 
 
 export function Plan() {
+
     const [options] = useState(PlanQuestions);
     const [drink, setDrink] = useState('____');
     const [type, setType] = useState('____');
     const [quantity, setQuantity] = useState('____');
     const [grind, setGrind] = useState('____');
     const [frequency, setFrequency] = useState('____');
-    const [price, setPrice] = useState(0);
+    const [payment, setPayment] = useState(0);
 
 
-    // Grind Option
-    const [isGrindDisabled, setIsGrindDisabled] = useState(false);
-
-    useEffect(() => {
-        if (drink === 'Capsule') {
-            setIsGrindDisabled(true);
-        } else {
-            setIsGrindDisabled(false);
-        }
-    }, [drink]);
-
-    // Modal 
-    const [showModal, setShowModal] = useState(false);
-
-    const showPrice = () => {
-        setShowModal(true);
+    // Calculate total shipping
+    const shippingCost = () => {
         switch (quantity) {
             case '250g':
-                return frequency === 'Every Week' ? setPrice('$28.20/mo') : frequency === 'Every 2 Weeks' ? setPrice('$19.20/mo') : setPrice('$12.00/mo');
+                return frequency === 'Every week' ? 7.20 * 4 : frequency === 'Every 2 weeks' ? 9.60 * 2 : 12.00;
                 break;
             case '500g':
-                return frequency === 'Every Week' ? setPrice('$52.00/mo') : frequency === 'Every 2 Weeks' ? setPrice('$35.00/mo') : setPrice('$22.00/mo');
+                return frequency === 'Every week' ? 13.00 * 4 : frequency === 'Every 2 weeks' ? 17.50 * 2 : 22.00;
                 break;
             case '1000g':
-                return frequency === 'Every Week' ? setPrice('$88.00/mo') : frequency === 'Every 2 Weeks' ? setPrice('$64.00/mo') : setPrice('$42.00/mo');
+                return frequency === 'Every week' ? 22.00 * 4 : frequency === 'Every 2 weeks' ? 32.00 * 2 : 42.00;
                 break;
             default:
-                return price;
+                return shippingCost;
         }
     }
-
-    // Active and Disabled Buttons
-    const disabledButton = drink !== "____" && type !== "____" && quantity !== "____" && frequency !== "____" ? false : true;
-
-    const activeButton = drink !== "____" && type !== "____" && quantity !== "____" && grind !== "____" && frequency !== "____" ? false : true;
-
-
 
 
     return (
@@ -64,44 +44,21 @@ export function Plan() {
             <div className="plan-page">
                 <PlanBanner />
                 <PlanHowSection />
-                <div className='selection-container'>
-                    <PlanMenu />
-                    <PlanAccordion option={options[0]} changeWord={(word) => setDrink(word)} />
-                    <PlanAccordion option={options[1]} changeWord={(word) => setType(word)} />
-                    <PlanAccordion option={options[2]} changeWord={(word) => setQuantity(word)} />
-                    {isGrindDisabled ? (
-                        <button className='option-button'>
-                            <h2 id={options.name} className='option-title' style={{ color: '#83888F', opacity: .05 }}>
-                                Want us to grind them?
-                            </h2>
+            </div>
+            <div className="selection-container">
+                <PlanMenu />
+                <PlanAccordion option={options[0]} changeWord={(word) => setDrink(word)} />
+                <PlanAccordion option={options[1]} changeWord={(word) => setType(word)} />
+                <PlanAccordion option={options[2]} changeWord={(word) => setQuantity(word)} />
+                <PlanAccordion option={options[3]} changeWord={(word) => setGrind(word)} />
+                <PlanAccordion option={options[4]} changeWord={(word) => setFrequency(word)} />
+            </div>
 
-                        </button>
-                    ) : (
-                        <PlanAccordion option={options[3]} changeWord={(word) => setGrind(word)} />
-                    )}
-
-                    <PlanAccordion option={options[4]} changeWord={(word) => setFrequency(word)} />
-                </div>
-
-                <div className='summary-container'>
-                    <PlanOrderSummary drink={drink} type={type} quantity={quantity} grind={grind} frequency={frequency} price={price} disabled={isGrindDisabled} />
-                    <div className="button-container">
-                        {isGrindDisabled ? (
-                            <button disabled={disabledButton} onClick={() => showPrice()} className='activated-order-btn disabled'>
-                                Create a plan
-                            </button>
-                        ) : (
-                            <button disabled={activeButton} onClick={() => showPrice()} className='activated-order-btn disabled'>
-                                Create a plan
-                            </button>
-                        )}
-                    </div>
-                </div>
-
-                <div className='modal-container'>
-                    <PlanCheckoutModal drink={drink} type={type} quantity={quantity} grind={grind} frequency={frequency} price={price} open={showModal} onClose={() => setShowModal(false)} />
-                </div>
-
+            <div className="summary-container">
+                <PlanOrderSummary drink={drink} type={type} quantity={quantity} grind={grind} frequency={frequency} />
+            </div>
+            <div className="checkout-container">
+                <PlanCheckoutModal drink={drink} type={type} quantity={quantity} grind={grind} frequency={frequency} shipping={shippingCost()} />
             </div>
         </>
     );
--- src/pages/Plan/PlanOrderSection/PlanOrderSummary.jsx
+++ src/pages/Plan/PlanOrderSection/PlanOrderSummary.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-export function PlanOrderSummary({ drink, type, quantity, grind, frequency }) {
+export function PlanOrderSummary(props) {
     return (
         <div className="order-summary-container">
             <div className="order-summary">
                 <span className="order-summary-title">Order Summary</span>
-                <p className="order-summary-text">
-                    "I drink my coffee <span className='summary-options'>{drink}</span>, with a <span className='summary-options'>{type}</span> type of bean. <span className='summary-options'>{quantity}</span> ground ala <span className='summary-options'>{grind}</span> , sent to me <span className='summary-options'>{frequency}</span>"
-                </p>
+                {props.disabled ? (
+                    <p className="order-summary-text">
+                        "I drink my coffee <span className='summary-options'>{props.drink}</span>, with a <span className='summary-options'>{props.type}</span> type of bean. <span className='summary-options'>{props.quantity}</span>, sent to me  <span className='summary-options'>{props.frequency}</span>"
+                    </p>
+                ) : (
+                    <p className="order-summary-text">
+                        "I drink my coffee <span className='summary-options'>{props.drink}</span>, with a <span className='summary-options'>{props.type}</span> type of bean. <span className='summary-options'>{props.quantity}</span> ground ala <span className='summary-options'>{props.grind}</span> , sent to me <span className='summary-options'> {props.frequency}</span>"
+                    </p>
+                )}
             </div>
         </div>
     );
