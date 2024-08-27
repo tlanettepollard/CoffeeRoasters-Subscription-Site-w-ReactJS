70df28faeb03200d23f0e832aae92df7cf0cd179
T. Lanette <TLPollard4516@gmail.com>
2024-08-27 16:16:53 -0400

    Update: redoing Plan page
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Plan/PlanOrderSection/PlanOrderSummary.jsx
+++ src/pages/Plan/PlanOrderSection/PlanOrderSummary.jsx
@@ -1,22 +1,2 @@
-import React from 'react';
 
-export function PlanOrderSummary(props) {
-    return (
-        <div className="order-summary-container">
-            <div className="order-summary">
-                <span className="order-summary-title">Order Summary</span>
-                {props.disabled ? (
-                    <p className="order-summary-text">
-                        "I drink my coffee <span className='summary-options'>{props.drink}</span>, with a <span className='summary-options'>{props.type}</span> type of bean. <span className='summary-options'>{props.quantity}</span>, sent to me  <span className='summary-options'>{props.frequency}</span>"
-                    </p>
-                ) : (
-                    <p className="order-summary-text">
-                        "I drink my coffee <span className='summary-options'>{props.drink}</span>, with a <span className='summary-options'>{props.type}</span> type of bean. <span className='summary-options'>{props.quantity}</span> ground ala <span className='summary-options'>{props.grind}</span> , sent to me <span className='summary-options'> {props.frequency}</span>"
-                    </p>
-                )}
-            </div>
-        </div>
-    );
-
-};
 
