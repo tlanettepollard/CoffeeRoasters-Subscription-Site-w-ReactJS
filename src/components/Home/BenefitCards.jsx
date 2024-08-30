80b841b6c84e15aee5195d6110d7cd7c6ff1cff3
T. Lanette <TLPollard4516@gmail.com>
2024-08-30 14:10:29 -0400

    Added Benefits and Collections sectios
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ src/components/Home/BenefitCards.jsx
@@ -0,0 +1,22 @@
+import { BenefitCards } from "../planData";
+
+export function BenefitCards() {
+    return (
+        <ul className='cards'>
+            {BenefitCards.map(card => {
+                return <li className='card' key={card.id}>
+                    <div className='card__container'>
+                        <div className='card__img__container'>
+                            <img className='card__img' src={card.imgUrl} alt={card.alt} />
+                        </div>
+                        <div className='card__info'>
+                            <h3 className='card__title'>{card.title}</h3>
+                            <p className='card__text'>{card.summary}</p>
+                        </div>
+                    </div>
+                </li>
+            })}
+        </ul>
+    );
+}
+
