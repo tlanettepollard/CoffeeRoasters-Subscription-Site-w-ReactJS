0d6d3b195323d01902ee4f5f037af66f172c1dab
T. Lanette <TLPollard4516@gmail.com>
2024-12-01 16:40:45 -0500

    Update: Order Summary
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Subscribe/OrderSummary.jsx
+++ src/components/Subscribe/OrderSummary.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-export function OrderSummary({ drink, type, quantity, grind, frequency }) {
+export function OrderSummary({ drink, beanType, qty, grind, frequency }) {
     return (
         <section className='order'>
             <form className='order__wrapper'>
                 <div className='order--summary'>
                     <h3 className="order--title">Order Summary</h3>
                     <p className="order--details">
-                        'I drink my coffee <span className='order--options'>{drink}</span> with a <span className='order--options'>{type}</span> type of bean, <span className='order--options'>{quantity}</span> ground ala <span className='order--options'>{grind}</span>, sent to me <span className='order--options'>{frequency}</span>.'
+                        'I drink my coffee <span className='order--options'>{drink}</span> with a <span className='order--options'>{beanType}</span> type of bean, <span className='order--options'>{qty}</span> ground ala <span className='order--options'>{grind}</span>, sent to me <span className='order--options'>{frequency}</span>.'
                     </p>
                 </div>
+                <button type='submit' className='order--btn disabled' title='proceed to checkout form'>Create your plan </button>
             </form>
         </section>
     );
