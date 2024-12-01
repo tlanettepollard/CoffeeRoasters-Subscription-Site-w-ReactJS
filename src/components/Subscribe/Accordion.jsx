0823215bc4a312b809088ef406d363422b067fd4
T. Lanette <TLPollard4516@gmail.com>
2024-12-01 16:14:42 -0500

    Updates: added Plan component
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

---
+++ src/components/Subscribe/Accordion.jsx
@@ -0,0 +1,5 @@
+import { useState, useEffect } from 'react';
+import { PlanQuestions } from '../planData';
+import Modal from '../Subscribe/Modal';
+import OrderSummary from '../Subscribe/OrderSummary';
+
---
+++ src/components/Subscribe/Plans.jsx
@@ -0,0 +1,11 @@
+import { AsideNav } from "./AsideNav";
+
+export function Plans() {
+    return (
+        <section className='plan__container'>
+            <div className="plan__section">
+                <AsideNav />
+            </div>
+        </section>
+    )
+}
