13ab2c4657312d55df656188edc52d481bc452d5
T. Lanette <TLPollard4516@gmail.com>
2024-12-04 18:53:17 -0500

    Update Accordion: added function for shipping cost
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/components/Subscribe/Accordion.jsx
+++ src/components/Subscribe/Accordion.jsx
@@ -35,11 +35,23 @@ export default function Accordion() {
 
     // calculate total shipping cost per month per weight
     const shippingCost = () => {
-        switch (quantity) {
+        switch (radioData.quantity) {
+            case '250g':
+                return radioData.delivery === 'Every week' ? 7.20 * 4 : radioData.delivery === 'Every 2 weeks' ? 9.60 * 2 : 12.00;
+                break;
+            case '500g':
+                return radioData.delivery === 'Every week' ? 13 * 4 : radioData.delivery === 'Every 2 weeks' ? 17.50 * 2 : 22.00;
+                break;
+            case '1000g':
+                return radioData.delivery === 'Every week' ? 22.00 * 4 : radioData.delivery === 'Every 2 weeks' ? 32.00 * 2 : 42.00;
+                break;
+            default:
+                return shippingCost;
+
 
         }
     }
-// Left off adding Switch statement for shipping
+    // Left off adding Switch statement for shipping
 
     return (
         <div className="order">
